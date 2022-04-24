5c5651b54f4f13e1d2d6941c69e8a62516efa101
bjimenezTechnodomus <bjimenez@technodomus.com>
2022-04-24 09:58:19 -0500

    Configuración del servicios Se configuraron los servicios de baterial y el de monitoreo ambiental por ahora solo con humedad relativa.

--- README.md
+++ README.md
@@ -19,12 +19,12 @@ La UMA es un sistema desarrollado para el monitoreo ambiental, permitiendo el al
     - [X] BH1750
     - [X] PMSA0031
     - [X] BME688
-    - [X] K30
+    - [X] K30 (a veces)
     - [ ] LOX2
-    - [ ] Adalogger
+    - [ ] Adalogger (funcionaba pero el porta tarjetas dejo de funcionar)
 - [ ] Obtener lecturas individuales y monitorearlas en el serial
     - [X] BH1750
-    - [ ] PMSA0031
+    - [X] PMSA0031
     - [X] BME688
     - [ ] K30
         - [ ] Recuperación del sensor cuando deja de registrar
@@ -53,11 +53,19 @@ La UMA es un sistema desarrollado para el monitoreo ambiental, permitiendo el al
 
 ### Microcontrolador
 
-El control se realiza con un chip ATSAMD21 que integra un modulo de Bluetooth el [Adafruit Feather con BTE](https://learn.adafruit.com/adafruit-feather-m0-bluefruit-le) 
+El control se realiza con un chip ATSAMD21 que integra un modulo de Bluetooth el [Adafruit Feather con BTE](https://learn.adafruit.com/adafruit-feather-m0-bluefruit-le).
 El modulo requiere de configuraciones especiales en la librería de Adafruit, para configurar el modulo SPI de hardware con los siguientes pines CS = 8, IRQ = 7, RST = 4.
 
 ### Librerías y configuración
 
 #### K30
 
-El K30 es un sensor muy usado, por lo que cuenta con varias librerías. Comenzamos las pruebas con [esta](https://github.com/FirstCypress/K30_CO2_I2C_Arduino)
+El K30 es un sensor muy usado, por lo que cuenta con varias librerías. Comenzamos las pruebas con [esta](https://github.com/FirstCypress/K30_CO2_I2C_Arduino). Sin embargo, es muy sensible al voltaje y parece que tiene problemas para funcionar con otros dispositivos en el mismo cable de I²C, arrojando valores nulos. En algunos casos se restaura después de provocar un [error](https://github.com/micropython/micropython/issues/1075) por exceso de conexiones sin reconocimiento (ACK o Acknowledge), pero no siempre funciona. Se puede consultar la documentación del sensor, pero sería necesario una analizador de señales para saber que es lo que esta pasando. [Guía de I2C](https://www.driesen-kern.de/downloads/ba-senseair-k30_k33-i2c.pdf).
+
+Otros usuarios han reportado problemas similares con este sensor que incluyen un comportamiento "impredecible" al momento de generar errores [aquí](https://forums.raspberrypi.com/viewtopic.php?t=162710)
+
+## Comunicación por BLE
+
+La idea original es que la UMA transmita por BLE la información de los sensores a un hub que los almacena y transmite a Internet. El hub es una Raspberry Pi 3 que transmite por cable Ethernet a otra RPi con acceso a internet. Parte del código se encuentra en estos repositorios [aquí el hub](https://github.com/mauriciobenjamin/setup_gateway), [aquí el servidor MQTT](https://github.com/mauriciobenjamin/mosquitto_rpivsrpi), [aquí en código del hub original](https://github.com/bjimenezTechnodomus/app_dataloggin).
+
+Hay que considerar que en las bibliotecas de Adafruit mencionan algunos problemas de usar una RPi 3B Rev 1.2 en la comunicación Bluetooth vía UART ya que el chip esta conectado al procesador principal sin control de flujo y hay que bajar la velocidad del UART editando `/usr/bin/btuart` pasando de `921600` a `460800`, [instrucciones aquí](https://github.com/adafruit/Adafruit_Blinka_bleio).
--- battery/battery.ino
+++ battery/battery.ino
@@ -1,13 +1,126 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "Adafruit_BLEBattery.h"
+#include "BluefruitConfig.h"
+#include "Adafruit_BME680.h"
+
 #define VBATPIN A7
-   
-void setup() {
+#define FACTORYRESET_ENABLE 1
+#define BME_CS A5
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+Adafruit_BLEBattery battery(ble);
+
+Adafruit_BME680 bme(BME_CS);
+
+void error(const __FlashStringHelper*err) {
+    Serial.println(err);
+    while (1);
 }
 
-void loop() {
+/* Información del servicio */
+int32_t maServiceId;
+int32_t maMeasureCharId;
+int32_t maLocationCharId;
+
+void setup() {
+    while (! Serial);
+    delay(500);
+    Serial.begin(115200);
+    Serial.println("Ejemplo con bateria y sensor de humedad");
+
+    if (! ble.begin(VERBOSE_MODE))
+    {
+        error(F("No se pudo encontrar Bluefruit"));
+    }
+    Serial.println("Modulo encontrado");
+
+    if (FACTORYRESET_ENABLE)
+    {
+        Serial.println(F("Realizando el borrado de fabrica"));
+        if (! ble.factoryReset() )
+        {
+            error(F("No fue posible reiniciar el segmento"));
+        }
+        
+    }
+
+    ble.echo(false);
+    Serial.println("Solicitando la información");
+    ble.info(); 
+
+    Serial.println("Modificando el nombre del dispositivo a 'UMAv0.1':");
+    if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=UMAv0.1")))
+    {
+        error(F("No se pudo configurar el nombre del dispositivo"));
+    }
+
+    Serial.println(F("Agregando la definición del servicio de monitoreo ambiental (UUID = 0x181A): "));
+    boolean success = ble.sendCommandWithIntReply(F("AT+GATTADDSERVICE=UUID=0x181A"), &maServiceId);
+    if (! success)
+    {
+        error(F("No se pudo crear el servico de monitoreo"));
+    }
+    Serial.println(F("Agregando la característica de humedad (UUID = 0x2A6F): "));
+    success = ble.sendCommandWithIntReply(F("AT+GATTADDCHAR=UUID=0x2A6F, PROPERTIES=0x10, MIN_LEN=2, VALUE=31.2, DESCRIPTION=Humidity sensor"), &maMeasureCharId);
+    if (! success)
+    {
+        error(F("No se pudo crear la característica de humedad"));
+    }
+    
+    /* Se tiene que reiniciar el dispositivo después de hacer cambios en los servicios para que estos tengan efecto*/
+    Serial.println(F("Realizando un reinicio de SW: "));
+    ble.reset();
+    Serial.print("\n");
+    battery.begin(true);
+
+    if (! bme.begin())
+    {
+        Serial.println("No se encontró el sensor BM688");
+        delay(1000);
+    } else
+    {
+        bme.setTemperatureOversampling(BME680_OS_8X); 
+        bme.setHumidityOversampling(BME680_OS_2X);
+        bme.setPressureOversampling(BME680_OS_4X);
+        bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+        bme.setGasHeater(320, 150); // 320*C for 150 ms
+    }
+    
+    
+}
+
+void loop(void) {
     float measuredvbat = analogRead(VBATPIN);
     measuredvbat *= 2;    // we divided by 2, so multiply back
     measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
     measuredvbat /= 1024; // convert to voltage
-    Serial.print("VBat: " ); Serial.println(measuredvbat);
-    delay(2000);
+    measuredvbat = measuredvbat/4.2 * 100;
+    
+    if (!bme.performReading())
+    {
+        Serial.println("Falló la lectura del sensor BM688");
+    } else {
+        Serial.print("Humedad: ");
+        Serial.print(bme.humidity);
+        Serial.println(" %");
+        ble.print(F("AT+GATTCHAR="));
+        ble.print(maMeasureCharId);
+        ble.print(F(",00-"));
+        ble.println(String(bme.humidity, 2));
+        if (!ble.waitForOK())
+        {
+            Serial.println("Se fallo al obtener una respuesta");
+        }
+        
+    }
+    
+    Serial.print("Carga de la bateria: " ); Serial.println(measuredvbat);
+    battery.update(measuredvbat);
+
+    delay(3000);
 }
